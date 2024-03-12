2a1079fb079266376513be3ce11dd5e0891e4b02
AdrianCR <adrianrmz917@gmail.com>
2024-03-12 17:24:50 -0600

    Files updated

--- register.php
+++ register.php
@@ -11,6 +11,39 @@
 <body>
     <div class="container">
         <div class="box form-box">
+
+        <?php
+        include("php/config.php");
+        if (isset($_POST['submit'])) {
+            $username =$_POST['username'];
+            $email =$_POST['email'];
+            $age =$_POST['age'];
+            $password =$_POST['password'];
+
+            // Verifying the unique email
+
+            $verify_query = mysqli_query($con, "SELECT Email FROM users WHERE Email = '$email'");
+
+            if (mysqli_num_rows($verify_query) != 0) {
+                echo "<div class= 'message'>
+                <p>This email is used, Try another one</p>
+                </div> <br>";
+                echo "<a href = 'javascript:self.history.back()'><button class = 'btn'>Go back</button>";
+
+            }
+            else{
+                mysqli_query($con, "INSERT INTO users(Username, Email, Age, Password) VALUES('$username', '$email', '$age', '$password')") or die ("Error Occured");
+                
+                echo "<div class= 'message'>
+                <p>Succesfully Registrated</p>
+                </div> <br>";
+                echo "<a href = 'index.php'><button class = 'btn'>Login Now</button>";
+            }
+        } else{
+            
+        
+        ?>
+
             <header>Sign Up</header>
             <form action="" method="post">
                 <div class="field input">
@@ -34,13 +67,14 @@
                 </div>
 
                 <div class="field">
-                    <input type="submit" class="btn" name="submit"  value="Login" required>
+                    <input type="submit" class="btn" name="Register"  value="Login" required>
                 </div>
                 <div class="links">
                     Alredy a member? <a href="index.php">Sign In</a>
                 </div>
             </form>
         </div>
+        <?php } ?>
     </div>
 </body>
 </html>
